dbb10c9db073aefe31e090bf31b718fc1c4ead6a
Yengibar Manasyan <yengibar.manasyan@sflpro.com>
2016-08-01 22:47:09 +0400

    Moved main activity to package com.healthiera.healthieramobile.activity.main

--- app/src/main/java/com/healthiera/healthieramobile/activity/main/MainActivity.java
+++ app/src/main/java/com/healthiera/healthieramobile/activity/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.healthiera.healthieramobile;
+package com.healthiera.healthieramobile.activity.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.healthiera.healthieramobile.R;
 
 public class MainActivity extends AppCompatActivity {
 
