dbe340896f0ec806d58aab23b71c76f3048aa4d8
Meline <meline.terarakelyan@gmail.com>
2016-08-12 00:53:37 +0400

    added custom components htEditText and htTextView

--- app/src/main/java/com/healthiera/mobile/htComponents/htTextView.java
+++ app/src/main/java/com/healthiera/mobile/htComponents/htTextView.java
@@ -1,7 +1,33 @@
 package com.healthiera.mobile.htComponents;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
 /**
- * Created by Meline on 12.08.2016.
+ * Created by Davit on 12.08.2016.
  */
-public class htTextView {
+public class htTextView extends TextView
+{
+    public htTextView(Context context) {
+        super(context);
+    }
+
+
+    public htTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public htTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+
+    protected void onDraw (Canvas canvas) {
+        super.onDraw(canvas);
+
+
+    }
 }
