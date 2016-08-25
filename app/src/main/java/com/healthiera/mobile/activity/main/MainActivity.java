02d566b2ff6b095e039e8e66f113779753ec2255
Yengibar Manasyan <yengibar.manasyan@sflpro.com>
2016-08-25 15:24:37 +0400

    Code formatting

--- app/src/main/java/com/healthiera/mobile/activity/main/MainActivity.java
+++ app/src/main/java/com/healthiera/mobile/activity/main/MainActivity.java
@@ -3,80 +3,41 @@ package com.healthiera.mobile.activity.main;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import com.healthiera.mobile.R;
-import com.healthiera.mobile.entity.Appointment;
-import com.healthiera.mobile.entity.Event;
-import com.healthiera.mobile.entity.Setting;
-import com.healthiera.mobile.mFragment.*;
-import com.healthiera.mobile.serivce.AppointmentService;
-import com.healthiera.mobile.serivce.EventService;
-
-import java.util.Date;
+import com.healthiera.mobile.mFragment.Calendar;
+import com.healthiera.mobile.mFragment.Dashboard;
+import com.healthiera.mobile.mFragment.Goals;
 
 public class MainActivity extends AppCompatActivity implements AHBottomNavigation.OnTabSelectedListener {
 
-    private EventService eventService = new EventService();
-
-    private AppointmentService appointmentService = new AppointmentService();
-
-    AHBottomNavigation bottomNavigation;
+    private AHBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        try {
-//            // TODO delete it, just a demo
-//            final Setting st = new Setting("Test_Key", "Test_Value");
-//            final Long settingId = st.save();
-//            final Setting loadedSetting = Setting.load(Setting.class, settingId);
-//            Log.i(loadedSetting.getKey(), loadedSetting.getValue());
-//            final Event event = new Event();
-//            event.setDescription("Event description");
-//            event.setStartDateTime(new Date());
-//            eventService.createAppointment(event);
-//            final Appointment appointment = new Appointment();
-//            appointment.setDoctorId(55L);
-//            appointment.setEvent(event);
-//            final Long savedAppointmentId = appointmentService.createAppointment(appointment);
-//            final Appointment foundAppointment = appointmentService.findAppointmentById(savedAppointmentId);
-//            System.out.println(foundAppointment.getDoctorId() + " " + appointment.getEvent().getDescription());
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-
-
-
-        //********************************************
-
         bottomNavigation = (AHBottomNavigation) findViewById(R.id.AHBottomNavigation_id);
         bottomNavigation.setOnTabSelectedListener(this);
         this.createNavItems();
-
-        //********************************************
-
     }
 
-    private void createNavItems()
-    {
+    private void createNavItems() {
         // Create items
-        AHBottomNavigationItem Dashboard = new AHBottomNavigationItem(R.string.dashboard, R.drawable.dashboard_icon,R.color.menu);
-        AHBottomNavigationItem Calendar = new AHBottomNavigationItem(R.string.calendar, R.drawable.calendar_icon,R.color.menu);
-        AHBottomNavigationItem Goals = new AHBottomNavigationItem(R.string.goals, R.drawable.goals_icon,R.color.menu);
+        final AHBottomNavigationItem dashboard = new AHBottomNavigationItem(R.string.dashboard, R.drawable.dashboard_icon, R.color.menu);
+        final AHBottomNavigationItem calendar = new AHBottomNavigationItem(R.string.calendar, R.drawable.calendar_icon, R.color.menu);
+        final AHBottomNavigationItem goals = new AHBottomNavigationItem(R.string.goals, R.drawable.goals_icon, R.color.menu);
 
         // Add items
-        bottomNavigation.addItem(Dashboard);
-        bottomNavigation.addItem(Calendar);
-        bottomNavigation.addItem(Goals);
+        bottomNavigation.addItem(dashboard);
+        bottomNavigation.addItem(calendar);
+        bottomNavigation.addItem(goals);
 
         // Set background color
         bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
-
     }
 
     /**
@@ -89,23 +50,15 @@ public class MainActivity extends AppCompatActivity implements AHBottomNavigatio
     @Override
     public boolean onTabSelected(int position, boolean wasSelected) {
         // Show fragments
-
-        if(position==0)
-        {
-            Dashboard dashboard =new Dashboard();
-            getSupportFragmentManager().beginTransaction().replace(R.id.Content_id,dashboard).commit();
-        }
-        else
-        if(position==1)
-        {
-            Calendar calendar =new Calendar();
-            getSupportFragmentManager().beginTransaction().replace(R.id.Content_id,calendar).commit();
-        }
-        else
-        if(position==2)
-        {
-            Goals goals =new Goals();
-            getSupportFragmentManager().beginTransaction().replace(R.id.Content_id,goals).commit();
+        if (position == 0) {
+            final Dashboard dashboard = new Dashboard();
+            getSupportFragmentManager().beginTransaction().replace(R.id.Content_id, dashboard).commit();
+        } else if (position == 1) {
+            Calendar calendar = new Calendar();
+            getSupportFragmentManager().beginTransaction().replace(R.id.Content_id, calendar).commit();
+        } else if (position == 2) {
+            Goals goals = new Goals();
+            getSupportFragmentManager().beginTransaction().replace(R.id.Content_id, goals).commit();
         }
 
         return true;
